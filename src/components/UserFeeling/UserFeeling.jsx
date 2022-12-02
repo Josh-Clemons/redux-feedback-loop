3cb0b85cd90ce39184b0ac14a69a4e7af6064ac6
Josh-Clemons <110069447+Josh-Clemons@users.noreply.github.com>
2022-12-02 16:21:07 -0600

    user feeling component logic completed

--- src/components/UserFeeling/UserFeeling.jsx
+++ src/components/UserFeeling/UserFeeling.jsx
@@ -1,9 +1,31 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
 export default function UserFeeling () {
+    const [feeling, setFeeling ] = useState('');
+    const history = useHistory();
+    const dispatch = useDispatch();
+
+    const handleClick = () => {
+        if (feeling === ''){
+            alert('Input a number silly');
+            return;
+        } else {
+            dispatch({
+                type: 'UPDATE_FEELING',
+                payload: feeling,
+            })
+            history.push('/understanding')
+        }
+    }
+
+
     return (
         <div>
             <h2>How are you feeling today?</h2>
-            <input type='number' />
-            <button>Next</button>
+            <input min="1" max="5" type='number' onChange={(e) => setFeeling(e.target.value)}/>
+            <button onClick={handleClick}>Next</button>
         </div>
     )
 }
