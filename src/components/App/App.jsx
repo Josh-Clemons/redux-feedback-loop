a2dcbd263de5cba7c492430a0a12f46a74952eef
Josh-Clemons <110069447+Josh-Clemons@users.noreply.github.com>
2022-12-02 15:48:50 -0600

    completed basic reducers on index.js

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import axios from 'axios';
+
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
+import {useDispatch} from 'react-redux';
+import {useState, useEffect} from 'react';
+
 import './App.css';
 
 function App() {
